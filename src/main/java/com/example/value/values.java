31d3418f3bbe3d4a6fd2e9585194fcbbab003341
keerthanamahalingam <keerthu2929@gmail.com>
2023-02-23 09:30:30 +0530

    First commit

---
+++ src/main/java/com/example/value/values.java
@@ -0,0 +1,19 @@
+
+package com.example.value;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class values {
+	
+@Value("${value}")
+private String name;
+
+@GetMapping("/val")
+public String display()
+{
+	return "Welcome to this "+name;
+}
+}
